6582a3c9a629ab9d2e75c0411f470a9875b4be94
Scott Bader <sb@scottbader.org>
2013-04-18 14:32:48 -0400

    adding metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "gotryiton"
+maintainer_email "scott@gotryiton.com"
+license          "Apache 2.0"
+description      "Installs libxml"
+long_description "Installs libxml"
+version          "0.1.0"
+
